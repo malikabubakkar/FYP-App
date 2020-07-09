9635664aee2f7fadd09ecd8feb8ae21d24f18e92
waqasjutt11 <54747067+waqasjutt11@users.noreply.github.com>
2020-07-09 20:12:39 +0500

    minor update

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import logo from './assets/logo.png';
-import firebase from 'firebase'
 export default function App() {
   
   return (
